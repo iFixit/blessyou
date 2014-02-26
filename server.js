a89584bf96ca9422dfbcf2ffc13c53a971c37e15
Daniel Beardsley <daniel@ifixit.com>
2014-02-26 11:09:09 -0700

    CLI: add pid-file option

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ http.createServer(function (req, res) {
 
 l("http server listening on: " + argv.host + ":" + argv.port); 
 
+if (argv['pid-file']) {
+   fs.writeFileSync(argv['pid-file'], process.pid)
+}
+
 function readEntireBody(stream) {
    var deferred = Q.defer()
    var body = ''
@@ -82,10 +86,11 @@ function round(num, places) {
 
 
 function args() {
-   return yargs.usage("Usage: $0 --port=<port> [--host=<host-ip>]")
+   return yargs.usage("Usage: $0 --port=<port> [--host=<host-ip>] [--pid-file=path]")
       .describe('port', 'tcp port to run the http server on.')
       .demand('port')
       .describe('host', 'tcp host to accept connections from.')
       .default('host', '127.0.0.1')
+      .describe('pid-file', 'file path to save the current pid in')
       .argv
 }
